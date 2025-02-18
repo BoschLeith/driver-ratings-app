8dc899ce412ff3a1c2df3aabd1be663e9db4f47b
BoschLeith <bosch.leith@gmail.com>
2025-02-18 10:25:21 -0600

    Add React Query and Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Driver {
+  name: string;
+  id: number;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
+
+const getDrivers = async () => {
+  const response = await axios.get("http://localhost:8080/api/drivers");
+  return response.data.data;
+};
+
 function App() {
+  const {
+    data: drivers,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["driversData"],
+    queryFn: getDrivers,
+  });
+
+  if (isLoading) {
+    return <div>Fetching posts...</div>;
+  }
+
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>Drivers</div>
+      <ul>
+        {drivers.map((driver: Driver) => (
+          <li key={driver.id}>{driver.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
