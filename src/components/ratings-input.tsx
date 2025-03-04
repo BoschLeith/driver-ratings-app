fb5848d488f073bdb0bac959031297b7097a1796
BoschLeith <bosch.leith@gmail.com>
2025-03-04 16:39:22 -0600

    Add RatingsInput component for dynamic rating input and integrate into Dashboard

---
+++ src/components/ratings-input.tsx
@@ -0,0 +1,67 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import { ApiResponse, Rater } from "../types";
+
+const getRaters = async () => {
+  const response = await axios.get<ApiResponse<Rater>>(
+    "http://localhost:8080/api/raters"
+  );
+  return response.data;
+};
+
+interface RatingsInputProps {
+  onRatingsChange: (ratings: { [key: number]: number }) => void;
+}
+
+export default function RatingsInput({ onRatingsChange }: RatingsInputProps) {
+  const [ratings, setRatings] = useState<{ [key: number]: number }>({});
+
+  const {
+    data: raters,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["ratersData"],
+    queryFn: getRaters,
+  });
+
+  useEffect(() => {
+    onRatingsChange(ratings);
+  }, [ratings, onRatingsChange]);
+
+  if (isLoading) {
+    return <p>Loading raters...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading raters.</p>;
+  }
+
+  const handleInputChange = (raterId: number, value: number) => {
+    setRatings((prevRatings) => ({
+      ...prevRatings,
+      [raterId]: value,
+    }));
+  };
+
+  return (
+    <>
+      {raters?.data.map((rater) => (
+        <fieldset className="fieldset" key={rater.id}>
+          <legend className="fieldset-legend">{rater.name}</legend>
+          <input
+            type="number"
+            className="input"
+            placeholder="Rating"
+            value={ratings[rater.id] || ""}
+            onChange={(e) =>
+              handleInputChange(rater.id, Number(e.target.value))
+            }
+          />
+        </fieldset>
+      ))}
+    </>
+  );
+}
--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,83 +1,88 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { usePulsy } from "pulsy";
 import { useState } from "react";
 
+import { logout } from "./auth-service";
 import DriverSelect from "./components/driver-select";
 import GrandPrixSelect from "./components/grand-prix-select";
 import PositionSelect from "./components/position-select";
 import RaceSelect from "./components/race-select";
+import RatingsInput from "./components/ratings-input";
 import TeamSelect from "./components/team-select";
 import {
   ApiResponse,
   InsertedResult,
   InsertRating,
   InsertResult,
-  Rater,
 } from "./types";
 
-const getRaters = async () => {
-  const response = await axios.get<ApiResponse<Rater>>(
-    "http://localhost:8080/api/raters"
-  );
-  return response.data;
-};
+const insertResult = async (
+  data: InsertResult
+): Promise<ApiResponse<InsertedResult>> => {
+  try {
+    const pulsyAuth = localStorage.getItem("pulsy_auth");
 
-// TODO: Check if pulsyAuth is null
-const insertResult = async (data: InsertResult) => {
-  const pulsyAuth = localStorage.getItem("pulsy_auth");
-  const parsedAuth = JSON.parse(pulsyAuth!);
-
-  const token = parsedAuth.value.token;
-
-  console.log(token);
-
-  const response = await axios.post<ApiResponse<InsertedResult>>(
-    "http://localhost:8080/api/results",
-    data,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+    if (!pulsyAuth) {
+      logout();
+      throw new Error("Authentication token not found");
     }
-  );
-  return response.data;
+
+    const token = JSON.parse(pulsyAuth).value.token;
+
+    const response = await axios.post<ApiResponse<InsertedResult>>(
+      "http://localhost:8080/api/results",
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error inserting result:", error);
+    throw error;
+  }
 };
 
 const insertRating = async (data: InsertRating[]) => {
-  const pulsyAuth = localStorage.getItem("pulsy_auth");
-  const parsedAuth = JSON.parse(pulsyAuth!);
+  try {
+    const pulsyAuth = localStorage.getItem("pulsy_auth");
 
-  const token = parsedAuth.value.token;
+    if (!pulsyAuth) {
+      logout();
+      throw new Error("Authentication token not found");
+    }
 
-  console.log(token);
+    const token = JSON.parse(pulsyAuth).value.token;
 
-  const response = await axios.post("http://localhost:8080/api/ratings", data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
+    const response = await axios.post(
+      "http://localhost:8080/api/ratings",
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error inserting ratings:", error);
+    throw error;
+  }
 };
 
 export default function Dashboard() {
   const [auth] = usePulsy<{ user: string }>("auth");
-  const [ratings, setRatings] = useState<{ [key: number]: number }>({});
-  const [position, setPosition] = useState<number | undefined>(undefined);
   const [driverId, setDriverId] = useState<number | null>(null);
-  const [teamId, setTeamId] = useState<number | null>(null);
   const [grandPrixId, setGrandPrixId] = useState<number | null>(null);
+  const [position, setPosition] = useState<number | null>(null);
   const [raceId, setRaceId] = useState<number | null>(null);
-
-  const {
-    data: raters,
-    error: ratersError,
-    isLoading: isLoadingRaters,
-  } = useQuery({
-    queryKey: ["ratersData"],
-    queryFn: getRaters,
-    enabled: !!grandPrixId && !!raceId,
-  });
+  const [ratings, setRatings] = useState<{ [key: number]: number }>({});
+  const [teamId, setTeamId] = useState<number | null>(null);
 
   const mutation = useMutation({
     mutationFn: insertResult,
@@ -98,31 +103,28 @@ export default function Dashboard() {
     },
   });
 
-  const handleInputChange = (raterId: number, value: number) => {
-    setRatings((prevRatings) => ({
-      ...prevRatings,
-      [raterId]: value,
-    }));
-  };
-
-  const handleGrandPrixChange = (grandPrixId: number | null) => {
-    setGrandPrixId(grandPrixId);
-  };
-
-  const handlePositionChange = (newPosition: number) => {
-    setPosition(newPosition);
-  };
-
   const handleDriverSelect = (driverId: number | null) => {
     setDriverId(driverId);
   };
 
-  const handleTeamSelect = (teamId: number | null) => {
-    setTeamId(teamId);
+  const handleGranPrixSelect = (grandPrixId: number | null) => {
+    setGrandPrixId(grandPrixId);
+  };
+
+  const handlePositionSelect = (newPosition: number | null) => {
+    setPosition(newPosition);
   };
 
   const handleRaceSelect = (raceId: number | null) => {
     setRaceId(raceId);
+  };
+
+  const handleRatingsChange = (newRatings: { [key: number]: number }) => {
+    setRatings(newRatings);
+  };
+
+  const handleTeamSelect = (teamId: number | null) => {
+    setTeamId(teamId);
   };
 
   // TODO: Add Validation
@@ -142,7 +144,7 @@ export default function Dashboard() {
         <h2>Welcome, {auth.user}!</h2>
       </div>
       <div>
-        <GrandPrixSelect onGrandPrixSelect={handleGrandPrixChange} />
+        <GrandPrixSelect onGrandPrixSelect={handleGranPrixSelect} />
 
         {grandPrixId && (
           <RaceSelect
@@ -151,29 +153,11 @@ export default function Dashboard() {
           />
         )}
 
-        <PositionSelect onPositionChange={handlePositionChange} />
+        <PositionSelect onPositionSelect={handlePositionSelect} />
         <DriverSelect onDriverSelect={handleDriverSelect} />
         <TeamSelect onTeamSelect={handleTeamSelect} />
-        {ratersError && (
-          <div className="fieldset-label text-error">{ratersError.message}</div>
-        )}
-        {isLoadingRaters && (
-          <div className="fieldset-label">Loading raters...</div>
-        )}
-        {raters?.data.map((rater) => (
-          <fieldset className="fieldset" key={rater.id}>
-            <legend className="fieldset-legend">{rater.name}</legend>
-            <input
-              type="number"
-              className="input"
-              placeholder="Rating"
-              value={ratings[rater.id] || ""}
-              onChange={(e) =>
-                handleInputChange(rater.id, Number(e.target.value))
-              }
-            />
-          </fieldset>
-        ))}
+        <RatingsInput onRatingsChange={handleRatingsChange} />
+
         {grandPrixId && <div>Grand Prix Id: {grandPrixId}</div>}
         {raceId && <div>Race Id: {raceId}</div>}
         {position && <div>Position: {position}</div>}
