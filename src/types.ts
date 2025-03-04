84f69e3dcfaaee85a02fba29b275c8e90648a5ad
BoschLeith <bosch.leith@gmail.com>
2025-03-04 09:24:07 -0600

    Move type definitions to a separate types file

---
+++ src/types.ts
@@ -0,0 +1,57 @@
+export interface GrandPrix {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Race {
+  id: number;
+  date: string;
+}
+
+export interface Driver {
+  id: number;
+  firstName: string;
+  lastName: string;
+  driverCode: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Rater {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+  fullName: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface InsertResult {
+  driverId: number;
+  teamId: number;
+  raceId: number;
+  position: number;
+}
+
+export interface InsertRating {
+  resultId: number;
+  raterId: number;
+  rating: number;
+}
+
+export interface InsertedResult {
+  id: number;
+}
+
+export interface ApiResponse<T> {
+  success: boolean;
+  data: T[];
+}
