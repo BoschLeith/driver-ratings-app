af5f6d2a6ca2e838d87c4b4115e46603ff289a99
BoschLeith <bosch.leith@gmail.com>
2025-03-28 11:18:28 -0500

    Refactor Teams component to use local state for data fetching and error handling; improve loading and error states display.

--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -1,47 +1,93 @@
-import { useTeamsQuery } from "../services/query-service";
+import { useEffect, useState } from "react";
+
+import { GET } from "../api/axios-instance";
+import { Team } from "../types/types";
 import { formatISODateTime } from "../utils/date-utils";
 
 export default function Teams() {
-  const { data: teams, isLoading, isError } = useTeamsQuery();
+  const [teams, setTeams] = useState<Team[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  // const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
 
-  if (isLoading) {
-    return <div>Fetching teams...</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const { success, data, message } = await GET<Team[]>("/teams");
+
+        if (!success || !data) {
+          throw new Error(message || "Failed to fetch team data.");
+        }
+
+        setTeams(data);
+      } catch (err) {
+        setError(
+          err instanceof Error ? err.message : "Unknown error occurred."
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center">
+        <span className="loading loading-spinner loading-xl"></span>
+      </div>
+    );
   }
 
-  if (isError) {
-    return <div>An error occurred while fetching teams.</div>;
-  }
-
-  if (!teams || teams.data.length === 0) {
-    return <div>No data found</div>;
+  if (error) {
+    return (
+      <div className="flex justify-center items-center p-4 text-red-600">
+        <p>Error: {error}</p>
+      </div>
+    );
   }
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Full Name</th>
-            <th>Name</th>
-            <th>Created At</th>
-            <th>Updated At</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teams.data.map((team) => (
-            <tr key={team.id}>
-              <td>{team.id}</td>
-              <td>{team.fullName}</td>
-              <td>{team.name}</td>
-              <td>{formatISODateTime(team.createdAt)}</td>
-              <td>
-                {team.updatedAt ? formatISODateTime(team.updatedAt) : "-"}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="card card-border bg-base-100 shadow-sm">
+      <div className="card-body">
+        <div className="flex items-center gap-4">
+          <h2 className="text-2xl font-bold tracking-tight">Races</h2>
+        </div>
+        {!teams || teams.length === 0 ? (
+          <p>No teams available.</p>
+        ) : (
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Full Name</th>
+                  <th>Name</th>
+                  <th>Created At</th>
+                  <th>Updated At</th>
+                </tr>
+              </thead>
+              <tbody>
+                {teams.map((team) => (
+                  <tr key={team.id}>
+                    <td>{team.id}</td>
+                    <td>{team.fullName}</td>
+                    <td>{team.name}</td>
+                    <td>{formatISODateTime(team.createdAt)}</td>
+                    <td>
+                      {team.updatedAt ? formatISODateTime(team.updatedAt) : "-"}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
